4f57e66db5339e456210a8f161f7699ab6de7d1f
Bill Allen <ballen@spectric.com>
2022-04-19 17:44:02 -0400

    Initial commit.

---
+++ install_vim_config.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+mkdir -p ~/.vim
+git clone https://github.com/VundleVim/Vundle.vim.git ~/.vim/bundle/Vundle.vim
+
+if [ -f ~/.vimrc]; then
+	cp ~/.vimrc ~/.vimrc.bak
+fi
+
+cp vimrc ~/.vimrc
